1d9b937ae1ba70652de4c68f2b0e0bc0e5eecbfe
FakriSouyo <fakriggwp@gmail.com>
2024-01-20 14:02:01 +0700

    fix

---
+++ app/src/main/java/com/example/pam1/constraint.kt
@@ -0,0 +1,11 @@
+package com.example.pam1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class constraint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
